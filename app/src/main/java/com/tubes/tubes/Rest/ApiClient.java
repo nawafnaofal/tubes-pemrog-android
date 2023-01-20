509226ab9829906a87eeebe85ab175e3022036ef
Nawaf Naofal <74226869+nawafnaofal@users.noreply.github.com>
2023-01-20 18:48:40 +0700

    Update ApiClient.java

--- app/src/main/java/com/tubes/tubes/Rest/ApiClient.java
+++ app/src/main/java/com/tubes/tubes/Rest/ApiClient.java
@@ -4,6 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
+    //IP Address nya sesuaikan dengan IP Address device yang digunakan, cara mengeceknya bisa dengan menggunakan cmd kemudian masukkan command ipconfig setelah itu akan muncul IP Address device yang digunakan lalu ganti "192.168.100.54" dengan IP Address device kalian.
     public static final String BASE_URL = "http://192.168.100.54/rest_ci/index.php/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
